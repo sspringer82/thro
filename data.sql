c6a27239dc3d9e1a3636c48d86d7e3e5afefe798
Sebastian Springer <info@sebastian-springer.com>
2019-11-11 15:49:20 +0100

    crud in sqlite

---
+++ data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE todo (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  done INTEGER
+);
+
+INSERT INTO todo (title, done) VALUES ('Get up', 1), ('eat', 0);
--- todo/model.js
+++ todo/model.js
@@ -1,25 +1,78 @@
 let todos = [{ id: 1, title: 'get up', done: false }];
-
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./data.sqlite3');
 module.exports = {
   create(todo) {
+    const cols = Object.keys(todo)
+      .map(k => `${k} = ?`)
+      .join(',');
+
+    return new Promise((res, rej) => {
+      db.run(
+        `INSERT INTO todo (${cols}) VALUES (${new Array(cols.length)
+          .fill('?')
+          .join(',')})`,
+        [...Object.values(todo)],
+        function(err, rows) {
+          if (err) {
+            rej(err);
+          }
+          res(rows);
+        }
+      );
+    });
+
     const newTodo = { ...todo, id: Math.max(...todos.map(t => t.id)) + 1 };
     todos.push(newTodo);
     return Promise.resolve(newTodo);
   },
   getAll() {
-    return Promise.resolve(todos);
+    return new Promise((res, rej) => {
+      db.all('SELECT * FROM todo', (err, rows) => {
+        if (err) {
+          rej(err);
+        }
+        res(rows);
+      });
+    });
   },
   getOne(id) {
-    const todo = todos.find(t => t.id === id);
-    return Promise.resolve(todo);
+    return new Promise((res, rej) => {
+      db.get('SELECT * FROM todo WHERE id = ?', [id], (err, rows) => {
+        if (err) {
+          rej(err);
+        }
+        res(rows);
+      });
+    });
   },
   update(id, todo) {
-    const index = todos.findIndex(t => t.id === id);
-    todos[index] = { ...todos[index], ...req.body };
-    return Promise.resolve(todos[index]);
+    const updated = todo;
+    const cols = Object.keys(updated)
+      .map(k => `${k} = ?`)
+      .join(',');
+
+    return new Promise((res, rej) => {
+      db.run(
+        `UPDATE todo SET ${cols} WHERE id = ?`,
+        [...Object.values(updated), id],
+        async (err, rows) => {
+          if (err) {
+            rej(err);
+          }
+          res(await this.getOne(id));
+        }
+      );
+    });
   },
   delete(id) {
-    todos = todos.filter(t => t.id !== id);
-    Promise.resolve();
+    return new Promise((res, rej) => {
+      db.run('DELETE FROM todo WHERE id = ?', [id], (err, rows) => {
+        if (err) {
+          rej(err);
+        }
+        res(rows);
+      });
+    });
   },
 };
